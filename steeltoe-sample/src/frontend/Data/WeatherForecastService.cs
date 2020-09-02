1d4b3efa8d1fac4ac82a238cf09d70a5f350c14f
Tim Heuer <tim@timheuer.com>
2020-09-02 15:11:52 -0700

    Adding blazor front-end app

---
+++ steeltoe-sample/src/frontend/Data/WeatherForecastService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace frontend.Data
+{
+    public class WeatherForecastService : ISolarSystemService
+    {
+        private const string SERVICE_URI = "https://solar-system-weather/weatherforecast";
+        private HttpClient _client;
+
+        public WeatherForecastService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IEnumerable<KeyValuePair<string, string>>> GetPlanetWeather()
+        {
+            return await _client.GetFromJsonAsync<IEnumerable<KeyValuePair<string, string>>>(SERVICE_URI);
+        }
+    }
+
+    public interface ISolarSystemService
+    {
+        Task<IEnumerable<KeyValuePair<string, string>>> GetPlanetWeather();
+    }
+}
